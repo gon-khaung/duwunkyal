cae6998121a76ac6b5fcca70e9cd830283a88ed8
gon-khaung <gonkhaung19@gmail.com>
2022-01-08 21:11:18 +0630

    fixed admin category

--- resources/js/pages/admin-dashboard/CategoryManagement.vue
+++ resources/js/pages/admin-dashboard/CategoryManagement.vue
@@ -1,85 +1,95 @@
 <template>
-  <div class="d-flex card">
-    <div class="col-lg-12 col-md-12 col-12 p-0">
-      <!-- Card header -->
-      <div class="card-header border-0 d-flex justify-content-between">
-        <h3 class="mb-0 pl-3">Categories Management</h3>
-        <div>
-          <button
-            type="button"
-            class="btn btn-outline-primary"
-            data-toggle="modal"
-            data-target="#new-account"
-          >
-            Add New Category
-          </button>
-        </div>
-      </div>
-      <div class="table betslip-table pb-3 table-responsive">
-        <DashboardTable
-          :tableData="tableData"
-          @data="getUserData"
-          @show="showModal"
-          type="category"
-        />
-      </div>
-    </div>
-    <div
-      class="modal fade"
-      id="new-account"
-      tabindex="-1"
-      role="dialog"
-      aria-labelledby="modal-form"
-      aria-hidden="true"
-    >
-      <div
-        class="modal-dialog modal- modal-dialog-centered modal-md"
-        role="document"
-      >
-        <div class="modal-content">
-          <div class="modal-body">
-            <div class="card bg-secondary border-0 mb-0">
-              <div class="card-body px-lg-5 py-lg-5 p-0">
-                <div class="pl-lg-4">
-                  <div class="row">
-                    <div class="col-lg-6">
-                      <div class="form-group">
-                        <label class="form-control-label">Image</label>
-
-                        <div class="custom-file d-flex">
-                          <van-uploader
-                            class="mx-auto"
-                            :max-count="1"
-                            v-model="imageUploader"
-                          />
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="pl-lg-4">
-                  <div class="form-group">
-                    <label class="form-control-label">Category Name</label>
-                    <input class="form-control" v-model="account.name" />
-                    <span class="text-danger">{{ errors.name }}</span>
-                  </div>
-                  <div class="form-group">
+    <div class="d-flex card">
+        <div class="col-lg-12 col-md-12 col-12 p-0">
+            <!-- Card header -->
+            <div class="card-header border-0 d-flex justify-content-between">
+                <h3 class="mb-0 pl-3">Categories Management</h3>
+                <div>
                     <button
-                      type="button"
-                      class="btn btn-primary"
-                      @click="sendNewAccount"
+                        type="button"
+                        class="btn btn-outline-primary"
+                        data-toggle="modal"
+                        data-target="#new-account"
                     >
-                      Confirm
+                        Add New Category
                     </button>
-                  </div>
                 </div>
-              </div>
             </div>
-          </div>
+            <div class="table betslip-table pb-3 table-responsive">
+                <DashboardTable
+                    :tableData="tableData"
+                    @data="getUserData"
+                    @show="showModal"
+                    type="categories"
+                />
+            </div>
         </div>
-      </div>
+        <div
+            class="modal fade"
+            id="new-account"
+            tabindex="-1"
+            role="dialog"
+            aria-labelledby="modal-form"
+            aria-hidden="true"
+        >
+            <div
+                class="modal-dialog modal- modal-dialog-centered modal-md"
+                role="document"
+            >
+                <div class="modal-content">
+                    <div class="modal-body">
+                        <div class="card bg-secondary border-0 mb-0">
+                            <div class="card-body px-lg-5 py-lg-5 p-0">
+                                <div class="pl-lg-4">
+                                    <div class="row">
+                                        <div class="col-lg-6">
+                                            <div class="form-group">
+                                                <label
+                                                    class="form-control-label"
+                                                    >Image</label
+                                                >
+
+                                                <div class="custom-file d-flex">
+                                                    <van-uploader
+                                                        class="mx-auto"
+                                                        :max-count="1"
+                                                        v-model="imageUploader"
+                                                    />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="pl-lg-4">
+                                    <div class="form-group">
+                                        <label class="form-control-label"
+                                            >Category Name</label
+                                        >
+                                        <input
+                                            class="form-control"
+                                            v-model="account.name"
+                                        />
+                                        <span class="text-danger">{{
+                                            errors.name
+                                        }}</span>
+                                    </div>
+                                    <div class="form-group">
+                                        <button
+                                            type="button"
+                                            class="btn btn-primary"
+                                            @click="sendNewAccount"
+                                        >
+                                            Confirm
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 <script>
 import { Toast, Dialog } from 'vant';
@@ -94,7 +104,7 @@ export default {
       account: {},
       isUpdate: false,
       tableData: {
-        headings: ['ID', 'Name'],
+        headings: ['ID', 'Name', 'Featured'],
         datas: [],
       },
     };
@@ -141,9 +151,9 @@ export default {
     getUserData(value) {
       Dialog.confirm({
         title:
-          this.$store.state.language === 'en'
-            ? 'Are you sure?'
-            : 'သေချာပါသလား?',
+                    this.$store.state.language === 'en'
+                      ? 'Are you sure?'
+                      : 'သေချာပါသလား?',
         cancelButtonText: 'Cancel',
         confirmButtonText: 'Sure',
       })
@@ -173,7 +183,7 @@ export default {
     },
     async fetchAccounts() {
       try {
-        this.tableData.contents = ['id', 'name'];
+        this.tableData.contents = ['id', 'name', 'is_featured'];
         const res = await axios.get('/categories');
         this.tableData.datas = res.data.data;
         this.rows = res.data.total;
@@ -189,9 +199,9 @@ export default {
 </script>
 <style scoped>
 .betslip-table {
-  background: white;
+    background: white;
 }
 .custom-file {
-  height: auto;
+    height: auto;
 }
 </style>
--- resources/js/pages/admin-dashboard/components/DashboardTable.vue
+++ resources/js/pages/admin-dashboard/components/DashboardTable.vue
@@ -1,161 +1,185 @@
 <template>
-  <div class="table-responsive">
-    <table class="table align-items-center" id="dashboard-select">
-      <thead class="thead-light">
-        <tr>
-          <th
-            v-for="(heading, index) in tableData.headings"
-            :key="index"
-            :class="index === 0 ? 'pl-5' : ''"
-          >
-            <div class="d-flex" v-if="index !== 0">
-              <div class="">{{ heading }}</div>
-            </div>
-            <div v-else>
-              {{ heading }}
-            </div>
-          </th>
-          <th>
-            <div class="d-flex">
-              <div class="mx-auto">Action</div>
-            </div>
-          </th>
-        </tr>
-      </thead>
-      <tbody class="list">
-        <tr v-for="(data, index) in tableData.datas" :key="index">
-          <th
-            scope="row"
-            v-for="(content, index) in tableData.contents"
-            :key="index"
-            :class="index === 0 ? 'pl-5' : ''"
-          >
-            <div class="media d-flex" v-if="content === 'status'">
-              <span class="mb-0 text-md">
-                <span
-                  class="badge badge-info"
-                  v-if="data[content] === 'Pending'"
-                >
-                  {{ data[content] }}
-                </span>
-                <span
-                  class="badge badge-success"
-                  v-if="data[content] === 'Delivered'"
-                >
-                  {{ data[content] }}
-                </span>
-                <span
-                  class="badge badge-danger"
-                  v-if="data[content] === 'Rejected'"
-                >
-                  {{ data[content] }}
-                </span>
-              </span>
-            </div>
-            <div class="media d-flex" v-else-if="content === 'isOpen'">
-              <span class="mb-0 text-md">
-                <span class="badge badge-danger" v-if="!data[content]"
-                  >Closed
-                </span>
-                <span class="badge badge-success" v-if="data[content]"
-                  >Running
-                </span>
-              </span>
-            </div>
-            <div class="media align-items-center" v-else>
-              <span class="name mb-0 text-md" v-if="!data['isEdit']">{{
-                data[content] ? data[content] : "-"
-              }}</span>
-              <span v-else
-                ><input
-                  id="input-email"
-                  class="form-control"
-                  v-model="data[content]"
-                  v-if="content !== 'roles'"
-                />
-                <v-select
-                  v-model="data[content]"
-                  :options="rolesSelection"
-                  v-else
-                ></v-select>
-              </span>
-            </div>
-          </th>
-          <th scope="row" class="action-row">
-            <div class="d-flex justify-content-end p-0" v-if="type === 'order'">
-              <button
-                type="button"
-                class="btn btn-success"
-                @click="typedData(data, index, 'deliver')"
-                v-if="data['status'] !== 'Delivered'"
-              >
-                Deliver
-              </button>
-              <button
-                type="button"
-                class="btn btn-danger"
-                @click="typedData(data, index, 'reject')"
-                v-if="data['status'] !== 'Delivered'"
-              >
-                Reject
-              </button>
-            </div>
+    <div class="table-responsive">
+        <table class="table align-items-center" id="dashboard-select">
+            <thead class="thead-light">
+                <tr>
+                    <th
+                        v-for="(heading, index) in tableData.headings"
+                        :key="index"
+                        :class="index === 0 ? 'pl-5' : ''"
+                    >
+                        <div class="d-flex" v-if="index !== 0">
+                            <div class="">{{ heading }}</div>
+                        </div>
+                        <div v-else>
+                            {{ heading }}
+                        </div>
+                    </th>
+                    <th>
+                        <div class="d-flex">
+                            <div class="mx-auto">Action</div>
+                        </div>
+                    </th>
+                </tr>
+            </thead>
+            <tbody class="list">
+                <tr v-for="(data, index) in tableData.datas" :key="index">
+                    <th
+                        scope="row"
+                        v-for="(content, index) in tableData.contents"
+                        :key="index"
+                        :class="index === 0 ? 'pl-5' : ''"
+                    >
+                        <div class="media d-flex" v-if="content === 'status'">
+                            <span class="mb-0 text-md">
+                                <span
+                                    class="badge badge-info"
+                                    v-if="data[content] === 'Pending'"
+                                >
+                                    {{ data[content] }}
+                                </span>
+                                <span
+                                    class="badge badge-success"
+                                    v-if="data[content] === 'Delivered'"
+                                >
+                                    {{ data[content] }}
+                                </span>
+                                <span
+                                    class="badge badge-danger"
+                                    v-if="data[content] === 'Rejected'"
+                                >
+                                    {{ data[content] }}
+                                </span>
+                            </span>
+                        </div>
+                        <div
+                            class="media d-flex"
+                            v-else-if="content === 'isOpen'"
+                        >
+                            <span class="mb-0 text-md">
+                                <span
+                                    class="badge badge-danger"
+                                    v-if="!data[content]"
+                                    >Closed
+                                </span>
+                                <span
+                                    class="badge badge-success"
+                                    v-if="data[content]"
+                                    >Running
+                                </span>
+                            </span>
+                        </div>
+                        <div
+                            class="media d-flex"
+                            v-else-if="content === 'is_featured'"
+                        >
+                            <input
+                                type="checkbox"
+                                name=""
+                                v-model="data[content]"
+                                @click.prevent="changeFeatured(data)"
+                            />
+                        </div>
+                        <div class="media align-items-center" v-else>
+                            <span
+                                class="name mb-0 text-md"
+                                v-if="!data['isEdit']"
+                                >{{ data[content] ? data[content] : "-" }}</span
+                            >
+                            <span v-else
+                                ><input
+                                    id="input-email"
+                                    class="form-control"
+                                    v-model="data[content]"
+                                    v-if="content !== 'roles'"
+                                />
+                                <v-select
+                                    v-model="data[content]"
+                                    :options="rolesSelection"
+                                    v-else
+                                ></v-select>
+                            </span>
+                        </div>
+                    </th>
+                    <th scope="row" class="action-row">
+                        <div
+                            class="d-flex justify-content-end p-0"
+                            v-if="type === 'order'"
+                        >
+                            <button
+                                type="button"
+                                class="btn btn-success"
+                                @click="typedData(data, index, 'deliver')"
+                                v-if="data['status'] !== 'Delivered'"
+                            >
+                                Deliver
+                            </button>
+                            <button
+                                type="button"
+                                class="btn btn-danger"
+                                @click="typedData(data, index, 'reject')"
+                                v-if="data['status'] !== 'Delivered'"
+                            >
+                                Reject
+                            </button>
+                        </div>
 
-            <div
-              class="d-flex justify-content-end p-0"
-              v-if="type === 'category'"
-            >
-              <button
-                type="button"
-                class="btn btn-info"
-                @click="showModal(data)"
-              >
-                Edit
-              </button>
-              <button
-                type="button"
-                class="btn btn-danger"
-                @click="typedData(data, index, 'delete')"
-              >
-                Delete
-              </button>
-            </div>
+                        <div
+                            class="d-flex justify-content-end p-0"
+                            v-if="type === 'categories'"
+                        >
+                            <button
+                                type="button"
+                                class="btn btn-info"
+                                @click="showModal(data)"
+                            >
+                                Edit
+                            </button>
+                            <button
+                                type="button"
+                                class="btn btn-danger"
+                                @click="typedData(data, index, 'delete')"
+                            >
+                                Delete
+                            </button>
+                        </div>
 
-            <div
-              class="d-flex justify-content-end p-0"
-              v-if="(type !== 'order') & (type !== 'category')"
-            >
-              <button
-                type="button"
-                class="btn btn-info"
-                @click="toEdit(index)"
-                v-if="!data['isEdit']"
-              >
-                Edit
-              </button>
-              <button
-                type="button"
-                class="btn btn-success"
-                @click="typedData(data, index, 'update')"
-                v-else
-              >
-                Confirm
-              </button>
-              <button
-                type="button"
-                class="btn btn-danger"
-                @click="typedData(data, index, 'delete')"
-              >
-                Delete
-              </button>
-            </div>
-          </th>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+                        <div
+                            class="d-flex justify-content-end p-0"
+                            v-if="(type !== 'order') & (type !== 'categories')"
+                        >
+                            <button
+                                type="button"
+                                class="btn btn-info"
+                                @click="toEdit(index)"
+                                v-if="!data['isEdit']"
+                            >
+                                Edit
+                            </button>
+                            <button
+                                type="button"
+                                class="btn btn-success"
+                                @click="typedData(data, index, 'update')"
+                                v-else
+                            >
+                                Confirm
+                            </button>
+                            <button
+                                type="button"
+                                class="btn btn-danger"
+                                @click="typedData(data, index, 'delete')"
+                            >
+                                Delete
+                            </button>
+                        </div>
+                    </th>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 <script>
+import { Toast, Dialog } from 'vant';
 import DashboardModal from './DashboardModal.vue';
 
 export default {
@@ -167,6 +191,33 @@ export default {
   props: ['tableData', 'type', 'transactionType'],
   components: { DashboardModal },
   methods: {
+    changeFeatured(data) {
+      Dialog.confirm({
+        title:
+                    data.is_featured === false
+                      ? 'Make this Featured Category?'
+                      : 'Removed this Featured Category?',
+        cancelButtonText: 'Cancel',
+        confirmButtonText: 'Sure',
+      })
+        .then(() => {
+          this.addOrRemoveNewData(data, 'is_featured');
+        })
+        .catch(() => {});
+    },
+    async addOrRemoveNewData(data, type) {
+      try {
+        const newData = data;
+        newData[type] = !newData[type];
+        const res = await axios.put(
+          `/${this.type}/${newData.id}`,
+          newData,
+        );
+        Toast.success(newData[type] === true ? 'Added!' : 'Removed!');
+      } catch (error) {
+        Toast.fail('Something wrong!');
+      }
+    },
     openModal(modalName) {
       $(`#${modalName}`).modal('show');
     },
@@ -192,9 +243,9 @@ export default {
 </script>
 <style scoped>
 .action-row {
-  width: 200px;
+    width: 200px;
 }
 .btn {
-  padding: 0.4rem 0.8rem;
+    padding: 0.4rem 0.8rem;
 }
 </style>
