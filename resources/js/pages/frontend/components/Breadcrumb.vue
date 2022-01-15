5c939aa5865d546ea526e7b952c37762766606fa
gon-khaung <gonkhaung19@gmail.com>
2022-01-15 21:02:43 +0630

    changed main image

--- resources/js/pages/frontend/components/Breadcrumb.vue
+++ resources/js/pages/frontend/components/Breadcrumb.vue
@@ -1,22 +1,20 @@
 <template>
-  <!-- Breadcrumb Section Begin -->
-  <section
-    class="breadcrumb-section set-bg"
-    style="
-      background-image: url('https://thetkhine.com/wp-content/uploads/2021/07/Sunset1-768x512.jpeg');
-    "
-  >
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-12 text-center">
-          <div class="breadcrumb__text">
-            <h2>{{ text }}</h2>
-          </div>
+    <!-- Breadcrumb Section Begin -->
+    <section
+        class="breadcrumb-section set-bg"
+        style="background: url('/build/assets/img/breadcum.jpeg')"
+    >
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="breadcrumb__text">
+                        <h2>{{ text }}</h2>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </section>
-  <!-- Breadcrumb Section End -->
+    </section>
+    <!-- Breadcrumb Section End -->
 </template>
 <script>
 export default {
